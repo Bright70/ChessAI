2ab7d85c9f69cea6732a59750fd7caae2952fbea
AianC <aechen0@gmail.com>
2016-04-01 15:04:57 -0600

    Recreated empty constructor for use in undoMove.

--- Java/Move.java
+++ Java/Move.java
@@ -1,15 +1,29 @@
+/*
+    Move class for ChessAI.
+*/
+
 package chessai;
 
 public class Move{
-	
-	int sx, ex, sy, ey;
-	Piece piece;
-	
-	Move(int sx, int ex, int sy, int ey, Piece piece){
-		this.sx = sx;
-		this.ex = ex;
-		this.sy = sy;
-		this.ey = ey;
-		this.piece = piece;
-	}
+
+    int sx, ex, sy, ey;
+    Piece piece;
+    
+    //empty constructor
+    Move(){
+        sx = -1;
+        ex = -1;
+        sy = -1;
+        ey = -1;
+        piece = new Empty();
+    }
+
+    //generate a Move given its information
+    Move(int sx, int ex, int sy, int ey, Piece piece){
+        this.sx = sx;
+        this.ex = ex;
+        this.sy = sy;
+        this.ey = ey;
+        this.piece = piece;
+    }
 }
