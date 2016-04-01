b43b6a0044a9fe2ee8179974bb2c4b3580707889
AianC <aechen0@gmail.com>
2016-04-01 15:03:14 -0600

    Improved UI.

--- Java/chessMain.java
+++ Java/chessMain.java
@@ -1,5 +1,5 @@
 /*
-    Temporary main for chessAI.
+    Temporary interface for chessAI.
  */
 
 package chessai;
@@ -8,26 +8,34 @@ import java.util.Scanner;
 
 public class chessMain
 {
-	
-	static Scanner in = new Scanner(System.in);
+    static Scanner in = new Scanner(System.in);
 	
     public static void main(String[] args)
     {
+        //create new game and vars
         ChessAI ai = new ChessAI();
         Board board = new Board();
         int sx, ex, sy, ey;
         board.displayBoard(board.board);
         
+        //loop for input
         do{
-        	sx = in.nextInt();
-	        ex = in.nextInt();
-	        sy = in.nextInt();
-	        ey = in.nextInt();
-        	
-        	Move move = new Move(sx, ex, sy, ey, board.board[sx][sy]);
-        	if(board.isLegal(move)){
-        		board.makeMove(move);
-        	}
-        }while(true);
+            System.out.print((board.turnCount % 2 == 0 ? "White" : "Black") + "'s turn: ");
+            sx = in.nextInt();
+            sy = in.nextInt();
+            ex = in.nextInt();
+            ey = in.nextInt();
+
+            Move move = new Move(sx, ex, sy, ey, board.board[sx][sy]);
+            
+            System.out.print("\n\n\n\n\n\n\n\n");
+            
+            if(board.isLegal(move))
+                board.makeMove(move);
+            else
+                System.out.print("Illegal move.");
+            
+            board.displayBoard(board.board);
+        } while(true); //win condition
     }
 }
